ee100909ba75cd0b8370650d9403552e7e9a5c1f
Denis Kornienko <korney197823@gmail.com>
2017-11-23 16:55:59 +0400

    Added effects styles and main.js file

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const card = document.querySelector('.card');
+const cardDescription = document.querySelector('.card__description');
+const productOrder = document.querySelector('.product-order');
+const buyButton = document.querySelector('.product-order__link'):
+const texts = {
+  description: 'Котэ не одобряет?',
+  liver: 'Печень утки разварная с артишоками.',
+  fish: 'Головы щучьи с чесноком да свежайшая сёмгушка.',
+  chicken: 'Филе из цыплят с трюфелями в бульоне.'
+};
